3b3115090b1853728cd8102f83add444973a289b
milanpatel41234 <mpatel41234@gmail.com>
2023-03-15 11:30:49 +0530

    try catch added

--- shellerspage.js
+++ shellerspage.js
@@ -4,57 +4,53 @@ let food = document.getElementById('food');
 let skincare = document.getElementById('skincare');
 let wearables = document.getElementById('wearables');
 
-electronics.addEventListener("click",removeItem);
-food.addEventListener("click",removeItem);
-skincare.addEventListener("click",removeItem);
-wearables.addEventListener("click",removeItem);
+electronics.addEventListener("click", removeItem);
+food.addEventListener("click", removeItem);
+skincare.addEventListener("click", removeItem);
+wearables.addEventListener("click", removeItem);
 
-{
-    axios.get("https://crudcrud.com/api/c78990caccc0441a8d2ed234b877c644/crud")
-    .then((res) =>{
-    
-        for(let i=0; i<res.data.length; i++){
+async function getdata() {
+    try {
+        let res = await axios.get("https://crudcrud.com/api/afbcb76495d547f480ca96da5635c6c1/crud")
+        for (let i = 0; i < res.data.length; i++) {
             let Obj = res.data[i];
             showuser(Obj)
         }
-    })
-    .catch((err) =>{
+    } catch (err) {
         console.log(err)
-    })  
+    }
 }
+getdata()
 
-function addproduct(e){
+async function addproduct(e) {
     e.preventDefault();
     let amount = document.getElementById('amount').value;
     let product = document.getElementById('product').value;
     let category = document.getElementById('category').value;
 
-      amount = amount.toString()
-      product = product.toString()
-      category = category.toString()
-    if(amount === "" || product === ""){
-     alert ('Please enter correct details !')
-    }else{
-     
-    //  let li = document.createElement('li')
-     const Obj = {
-        amount,
-        product,
-        category
-     }
-     
-    axios.post("https://crudcrud.com/api/c78990caccc0441a8d2ed234b877c644/crud",Obj)
-    .then((res) =>{
-        let Obj = res.data;
-        showuser(Obj)
-    })
-    .catch((err) =>{
-        console.log(err)
-    })   
+    amount = amount.toString()
+    product = product.toString()
+    category = category.toString()
+    if (amount === "" || product === "") {
+        alert('Please enter correct details !')
+    } else {
+        try {
+            const Obj = {
+                amount,
+                product,
+                category
+            }
+            let res = await axios.post("https://crudcrud.com/api/afbcb76495d547f480ca96da5635c6c1/crud", Obj)
+            let ans = res.data;
+            showuser(ans)
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
-function  showuser(Obj){
+function showuser(Obj) {
     let li = document.createElement("li");
+
     li.id = Obj._id;
     let amount = document.createTextNode(Obj.amount);
     li.appendChild(amount);
@@ -64,43 +60,41 @@ function  showuser(Obj){
     li.appendChild(document.createTextNode(' - '));
     let category = document.createTextNode(Obj.category);
     li.appendChild(category);
-  
-     // create delete button
-     let deleteBtn = document.createElement('button');
-     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-     deleteBtn.appendChild(document.createTextNode('delete'));
-     li.appendChild(deleteBtn);
-  
-   if (Obj.category === "electonics") {
-    electronics.appendChild(li)
-   } else if(Obj.category === "food") {
-    food.appendChild(li)
-   } else if(Obj.category === "skincare") {
-    skincare.appendChild(li)
-   }else if(Obj.category === "wearables") {
-    wearables.appendChild(li)
-   }
-  }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
+    // create delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deleteBtn);
+    if (Obj.category === "electronics") {
+        electronics.appendChild(li)
+    } else if (Obj.category === "food") {
+        food.appendChild(li)
+    } else if (Obj.category === "skincare") {
+        skincare.appendChild(li)
+    } else if (Obj.category === "wearables") {
+        wearables.appendChild(li)
+    }
+}
+
+async function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
         let li = e.target.parentElement;
         let id = li.id
-        axios.delete(`https://crudcrud.com/api/c78990caccc0441a8d2ed234b877c644/crud/${id}`)
-        .then((res) =>{
+        try {
+            let res = await axios.delete(`https://crudcrud.com/api/afbcb76495d547f480ca96da5635c6c1/crud/${id}`)
             const id1 = li.parentElement.id
-            if (id1 === "electonics") {
+            if (id1 === "electronics") {
                 electronics.removeChild(li)
-               } else if(id1 === "food") {
+            } else if (id1 === "food") {
                 food.removeChild(li)
-               } else if(id1 === "skincare") {
+            } else if (id1 === "skincare") {
                 skincare.removeChild(li)
-               }else if(id1 === "wearables") {
+            } else if (id1 === "wearables") {
                 wearables.removeChild(li)
-               }
-        })
-        .catch((err) =>{
+            }
+        } catch (err) {
             console.log(err)
-        })     
-     }
+        }
+    }
 }
